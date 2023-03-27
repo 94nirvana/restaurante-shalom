c7ad20f9037f1125db7e79402c6da5d1c2f54b5b
94nirvana <robertoprado89@gmail.com>
2023-03-27 11:12:22 -0300

    mudando

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s2zux*x&ge*9182np^t)3#r3z_rzzizd4&z#+g0y%nswwxze$l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
